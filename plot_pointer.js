6bfc3ab7a955b09d5b59b127b9a68a24fa6daddf
xperimex <adi@mittal.net>
2014-12-31 11:59:00 -0800

    Update plot_pointer.js

--- plot_pointer.js
+++ plot_pointer.js
@@ -2,19 +2,19 @@ function a(numPoints){
     for(var i = 1; i <= numPoints; i++){
     var x = i;
     
-    var z = Math.pow(10, 1/x);
+    var z = Math.pow(10, 1/x);  //incase exponents are in the equation
         
-    var y = 1/(1+z);
+    var y = 1/(1+z);    //equation
     
     var x1 = i*(-1);
 
     var y1 = y*(-1);
     
-    console.log(x, y);  //replace x w/ x1 & z w/ y1 to get negatives
+    console.log(x, y);  //replace x w/ x1 & y w/ y1 to get negatives
     }
     
     
     }
     
-a(15);
+a(15); //the max. number on the x coordinate (i.e. 1-15)
     
